006d9c58775265817880d8c6ed89aed8d948f8d5
sushma Narayana Swamy <susnar0626@edu.lerum.se>
2023-02-05 20:44:01 +0100

    task5

--- src/lms126/task5.py
+++ src/lms126/task5.py
@@ -2,4 +2,15 @@ from to_do import TODO
 
 
 def task5():
-    return TODO("Erase this line and write your solution")
+    return
+"""
+input enter the value for x
+if x= 1 the output= Monday
+if x= 2 the output= Tuesday
+if x= 3 the output= Wednesday
+if x= 4 the output= Thursday
+if x= 5 the output= Friday
+if x= 6 the output= Saturday
+if x= 7 the output= Sunday
+if x= 8 the output= Monday
+"""
