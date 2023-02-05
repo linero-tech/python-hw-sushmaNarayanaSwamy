488540168c1c8fea98470df53c7bdd6cce359baf
sushma Narayana Swamy <susnar0626@edu.lerum.se>
2023-02-05 22:41:37 +0100

    task 7

--- src/lms126/task7.py
+++ src/lms126/task7.py
@@ -2,4 +2,11 @@ from to_do import TODO
 
 
 def task7():
-    return TODO("Erase this line and write your solution")
+    return
+"""
+input the value of x
+if x is > than = 1 and < than 100
+then divide the number by 2
+if the value is integer 
+then output x 
+"""
