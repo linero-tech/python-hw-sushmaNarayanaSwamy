56646346096031daae009af6ad64ad645364d849
sushma Narayana Swamy <susnar0626@edu.lerum.se>
2023-02-05 17:58:24 +0100

    task 3

--- src/lms126/task3.py
+++ src/lms126/task3.py
@@ -2,9 +2,9 @@ from to_do import TODO
 
 
 def task3():
-    return TODO
-"""
+    return """
 Input base of the triangle
 input height of the triangle
 area is height * base divided by 2
-output area of the triangle"""
+output area of the triangle
+"""
