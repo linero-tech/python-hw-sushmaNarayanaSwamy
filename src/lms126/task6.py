fafcda6853b43b7308dfdcea89e8cf90baf1b465
sushma Narayana Swamy <susnar0626@edu.lerum.se>
2023-02-05 22:08:57 +0100

    task 6

--- src/lms126/task6.py
+++ src/lms126/task6.py
@@ -2,4 +2,11 @@ from to_do import TODO
 
 
 def task6():
-    return TODO("Erase this line and write your solution")
+    return
+"""
+input enter the value for X
+if x is > than = 1 and < than 100
+then divide the number by 5 
+if the value is a integer
+output the value of x
+"""
--- src/lms126/task8.py
+++ src/lms126/task8.py
@@ -1,3 +1,4 @@
+
 from to_do import TODO
 
 
