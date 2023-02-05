74d472d4b60d3651118ef0bf1bd9bab5a4e05d79
sushma Narayana Swamy <susnar0626@edu.lerum.se>
2023-02-05 22:30:51 +0100

    task 6

--- src/lms126/task6.py
+++ src/lms126/task6.py
@@ -4,9 +4,9 @@ from to_do import TODO
 def task6():
     return
 """
-input enter the value for X
+input enter the value for x
 if x is > than = 1 and < than 100
 then divide the number by 5 
 if the value is a integer
-output the value of x
+output the value of x  
 """
