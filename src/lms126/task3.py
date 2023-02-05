9170a8c4c6b238704ad599c9ffba094092fc87dc
sushma Narayana Swamy <susnar0626@edu.lerum.se>
2023-02-05 17:53:23 +0100

    Task 3

--- src/lms126/task3.py
+++ src/lms126/task3.py
@@ -2,4 +2,9 @@ from to_do import TODO
 
 
 def task3():
-    return TODO("Erase this line and write your solution")
+    return TODO
+"""
+Input base of the triangle
+input height of the triangle
+area is height * base divided by 2
+output area of the triangle"""
