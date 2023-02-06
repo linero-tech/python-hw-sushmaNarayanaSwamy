ccebe242d8a1f33aee8dfbc4373811f9e26a1297
sushma Narayana Swamy <susnar0626@edu.lerum.se>
2023-02-06 13:13:42 +0100

    task 8

--- src/lms126/task8.py
+++ src/lms126/task8.py
@@ -5,13 +5,12 @@ from to_do import TODO
 def task8():
     return """
     input enter the value of x 
-    if  x/ 3 s a integer and x/5 is a integer then 
+    if  x/ 3 is a integer and x/5 is a integer then 
     output print  FooBar
-    else if x/3 is not integer and   x/5 is not integer then 
+    else if x/3 is not integer and x/5 is not integer then 
     Output print Qix 
-    else  if  x/ 3 s a integer 
+    else  if  x/ 3 is a integer 
     output print Foo
-    else x/5 is a integer then
+    else if x/5 is a integer then
     output print Bar     
-     """
-
+    """
