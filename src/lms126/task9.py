c41367f7b608d2002a1f14c26d5f19330b561207
sushma Narayana Swamy <susnar0626@edu.lerum.se>
2023-02-06 20:14:49 +0100

    task 9

--- src/lms126/task9.py
+++ src/lms126/task9.py
@@ -2,4 +2,12 @@ from to_do import TODO
 
 
 def task9():
-    return TODO("Erase this line and write your solution")
+    return """
+    input insert the card and enter the pin
+    check if the card number and pin is validated 
+    if both is correct then show the account details
+    then enter the withdrawal amount 
+    withdrawal amount <= to balance amount in account
+    output the amount and display balance = Actual Balance-withdrawn money
+    else print insufficient balance cannot withdraw
+    """
