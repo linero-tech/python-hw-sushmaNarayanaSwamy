23cd9223248b6c57483f69dc3c98e33f12373673
sushma Narayana Swamy <susnar0626@edu.lerum.se>
2023-02-06 20:39:08 +0100

    task 7

--- src/lms126/task7.py
+++ src/lms126/task7.py
@@ -3,9 +3,9 @@ from to_do import TODO
 
 def task7():
     return """
-input the value of x
-if x is > than = 1 and < than 100
-then divide the number by 2
-if the value is integer 
-then output print x 
-"""
+    input the value of x
+    if x is > than = 1 and < than 100
+    then divide the number by 2
+    if the value is integer 
+    then output print x 
+    """
