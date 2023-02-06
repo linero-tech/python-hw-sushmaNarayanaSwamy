3326c3d122e2da14486bd5622635d91f8a70021b
sushma Narayana Swamy <susnar0626@edu.lerum.se>
2023-02-06 10:13:08 +0100

    task 8

--- src/lms126/task8.py
+++ src/lms126/task8.py
@@ -3,4 +3,15 @@ from to_do import TODO
 
 
 def task8():
-    return TODO("Erase this line and write your solution")
+    return """
+    input enter the value of x 
+    if  x/ 3 s a integer and x/5 is a integer then 
+    output print  FooBar
+    else if x/3 is not integer and   x/5 is not integer then 
+    Output print Qix 
+    else  if  x/ 3 s a integer 
+    output print Foo
+    else x/5 is a integer then
+    output print Bar     
+     """
+
