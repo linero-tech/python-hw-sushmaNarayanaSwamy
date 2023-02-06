88a1721464bfcf52204e8eca56639b6472f38854
sushma Narayana Swamy <susnar0626@edu.lerum.se>
2023-02-06 20:33:39 +0100

    task 11

--- src/lms126/task11.py
+++ src/lms126/task11.py
@@ -2,4 +2,9 @@ from to_do import TODO
 
 
 def task11():
-    return TODO("Erase this line and write your solution")
+    return """
+    Enter the value of x=temperature
+    check if x is in Celsius then print output =(temperature*9/5)+32
+    else
+    check if x is in Fahrenheit print output=(temperature-32)*5/9
+    """
