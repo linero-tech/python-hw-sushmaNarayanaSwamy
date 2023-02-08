cee26be1b423962026dcaed2de2eec3ea24f8d83
sushma Narayana Swamy <susnar0626@edu.lerum.se>
2023-02-08 17:27:27 +0100

    task 5

--- src/lms126/task5.py
+++ src/lms126/task5.py
@@ -5,11 +5,11 @@ def task5():
     return """
 input enter the value for x
 if x= 1 then output= Monday
-if x= 2 then output= Tuesday
-if x= 3 then output= Wednesday
-if x= 4 then output= Thursday
-if x= 5 then output= Friday
-if x= 6 then output= Saturday
-if x= 7 then output= Sunday
-if x= 8 then output= Monday
+else if x= 2 then output= Tuesday
+else if x= 3 then output= Wednesday
+else if x= 4 then output= Thursday
+else if x= 5 then output= Friday
+else if x= 6 then output= Saturday
+else if x= 7 then output= Sunday
+else if x= 8 then output= Monday
 """
