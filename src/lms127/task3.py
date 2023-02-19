ac39a1c427ab50d5fec044ffca85066e55646b4b
sushma Narayana Swamy <susnar0626@edu.lerum.se>
2023-02-20 00:42:17 +0100

    task 3

--- src/lms127/task3.py
+++ src/lms127/task3.py
@@ -2,6 +2,8 @@ from to_do import TODO
 
 
 def task3(radius):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+    result = 2 * 3.1415 * radius
+    return result
+if __name__ == "__main__":
+       result = task3(2)
+       print(result)
