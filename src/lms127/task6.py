1b9bfce596bfc9cc3e7963e927d8828057ddaa77
sushma Narayana Swamy <susnar0626@edu.lerum.se>
2023-02-20 23:50:14 +0100

    task 6

--- src/lms127/task6.py
+++ src/lms127/task6.py
@@ -2,6 +2,9 @@ from to_do import TODO
 
 
 def task6(base1, base2, height):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+    result = (base1 + base2) * height * 0.5
+    return result
+
+
+if __name__ == "__main__":
+    print(task6(4, 6, 6))
