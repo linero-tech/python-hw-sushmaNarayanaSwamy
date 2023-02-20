f78eabce141f38b8392f1272e5da1a00e7a50dbe
sushma Narayana Swamy <susnar0626@edu.lerum.se>
2023-02-20 23:56:52 +0100

    task 7

--- src/lms127/task7.py
+++ src/lms127/task7.py
@@ -2,6 +2,9 @@ from to_do import TODO
 
 
 def task7(sentence):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+    result = len(sentence)
+    return result
+
+
+if __name__ == "__main__":
+    print(task7("I like Music"))
