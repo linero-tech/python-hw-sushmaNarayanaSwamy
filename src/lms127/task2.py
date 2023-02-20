a423d6a4974f9764a857c0eb779e884314905ece
sushma Narayana Swamy <susnar0626@edu.lerum.se>
2023-02-20 23:38:10 +0100

    task 2

--- src/lms127/task2.py
+++ src/lms127/task2.py
@@ -1,10 +1,12 @@
 from to_do import TODO
 
+
 def task2():
     name = "Sushma"
     country = "India"
     month = "June"
     print(f"Hello {name}, you are from {country} and you are born in {month}")
+
 
 
 if __name__ == "__main__":
