e73d8d3a28a191a24a01c0af46720863b4bad8cf
sushma Narayana Swamy <susnar0626@edu.lerum.se>
2023-02-20 22:24:30 +0100

    task 5

--- src/lms127/task5.py
+++ src/lms127/task5.py
@@ -2,10 +2,12 @@ from to_do import TODO
 
 
 def task5(value_for_a, value_for_b):
-    a = value_for_a
-    b = value_for_b
+    temp = value_for_a
+    value_for_a= value_for_b
+    value_for_b = temp
+    print(f"a is {value_for_a}\nb is {value_for_b}")
+    return value_for_a, value_for_b
 
-    TODO("Your solutions goes in here.")
 
-    # Do not erase or change the below statement
-    return a, b
+if __name__ == "__main__":
+    task5(4,6)
