bdc6ea0c7f1bbfb974415dfa55d7eaed42e28e03
sushma Narayana Swamy <susnar0626@edu.lerum.se>
2023-02-20 01:06:21 +0100

    task 4

--- src/lms127/task4.py
+++ src/lms127/task4.py
@@ -4,6 +4,5 @@ from to_do import TODO
 def task4(base, height):
     area= 0.5 * base * height
     return area
-
 if __name__ == "__main__":
-     print(task4(base=2.0,height=4.0))
+     print(task4(base=2.0,height=10.0))
