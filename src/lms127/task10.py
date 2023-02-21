1b514ef49626c5ac21fa3b2a20153bcde9328337
sushma Narayana Swamy <susnar0626@edu.lerum.se>
2023-02-21 01:08:55 +0100

    task 10

--- src/lms127/task10.py
+++ src/lms127/task10.py
@@ -2,24 +2,24 @@ from to_do import TODO
 
 
 def task10_1(assessments):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
-
+    result1 = len(assessments)
+    return result1
 
 def task10_2(assessments):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
-
+    result2 = assessments[3]
+    return result2
 
 def task10_3(assessments):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+    result3 = len(assessments) / 2
+    return result3
 
 
 def task10_4(assessments):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+    return result4
+
+
+if __name__ == "__main__":
+    print(task10_1("ABCDE"))
+    print(task10_2("ABCDE"))
+    print(task10_3("ABCDE"))
+    print(task10_4("ABCDE"))
