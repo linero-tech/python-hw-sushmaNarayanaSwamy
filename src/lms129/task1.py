3364595001e0bd954d1158222e88375515a008d5
sushma Narayana Swamy <susnar0626@edu.lerum.se>
2023-03-07 18:16:57 +0100

    task 1

--- src/lms129/task1.py
+++ src/lms129/task1.py
@@ -1,7 +1,17 @@
 from to_do import TODO
 
-
 def task1(a, b):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+    if a >= b:
+        result = 0
+    else:
+        result = 0
+        for i in range(a, b+1):
+            result += i
+
+    print(result)
+    return result
+
+if __name__ == "__main__":
+    task1(1, 5)
+    task1(3, 6)
+    task1(3, 3)
