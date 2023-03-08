fad30340b99377df1452060ac7978337e0c35b71
sushma Narayana Swamy <susnar0626@edu.lerum.se>
2023-03-08 17:36:52 +0100

    task 8

--- src/lms129/task8.py
+++ src/lms129/task8.py
@@ -2,6 +2,13 @@ from to_do import TODO
 
 
 def task8(number):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+    result = 0
+    for digit in str(number):
+        result += int(digit)
+
+    return result
+
+
+if __name__ == "__main__":
+    print(task8(number=123))
+    print(task8(number= 444))
