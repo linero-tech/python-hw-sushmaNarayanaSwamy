1d5028787bf27d55a4832a324e49ab8a1ee08e02
sushma Narayana Swamy <susnar0626@edu.lerum.se>
2023-03-11 15:10:07 +0100

    Update task3.py
    task 3

--- src/lms129/task3.py
+++ src/lms129/task3.py
@@ -3,17 +3,15 @@ from to_do import TODO
 
 def task3(number):
     result = 1
-    if number == 0:
-        result = 1
-    else:
-        for i in range(1, number+1):
-            result = result*i
 
-    return print(result)
+    for i in range(1, number+1):
+        result = result*i
+
+    return result
 
 
 if __name__ == "__main__":
-    task3(5)
-    task3(4)
-    task3(3)
-    task3(8)
+    print(task3(0))
+    print(task3(4))
+    print(task3(3))
+    print(task3(5))
