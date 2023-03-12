ead13653327bf930f8202a8b07a6eac7707bb02c
sushma Narayana Swamy <susnar0626@edu.lerum.se>
2023-03-12 20:08:59 +0100

    task 6

--- src/lms129/task6.py
+++ src/lms129/task6.py
@@ -2,6 +2,14 @@ from to_do import TODO
 
 
 def task6(number):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+    result = ""
+    for i in str(number):
+        result = i + result
+
+    return print(int(result))
+
+
+if __name__ == "__main__":
+    task6(678)
+    task6(432)
+    task6(234)
