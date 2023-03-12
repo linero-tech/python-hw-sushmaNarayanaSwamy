5317d962b35a1018f6ca3752640c8eca10f3df1d
sushma Narayana Swamy <susnar0626@edu.lerum.se>
2023-03-12 20:24:04 +0100

    update task 7

--- src/lms129/task7.py
+++ src/lms129/task7.py
@@ -2,9 +2,24 @@ from to_do import TODO
 
 
 def task7(a, b):
+    result = 1
     if a and b == 0:
         result = 1
-
+    elif b == 1:
+        result = a
     else:
-        result and
+        for power in range(b):
+            result = result * a
+
+    print(result)
     return result
+
+
+if __name__ == "__main__":
+    task7(2, 4)
+    task7(6, 4)
+    task7(4, 4)
+    task7(3, 4)
+    task7(10, 4)
+    task7(20, 4)
+
