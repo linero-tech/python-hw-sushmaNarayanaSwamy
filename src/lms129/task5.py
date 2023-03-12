ece6098df3a6afa380e496dbd48ff13b58abe77e
sushma Narayana Swamy <susnar0626@edu.lerum.se>
2023-03-12 20:39:29 +0100

    task 5 latest

--- src/lms129/task5.py
+++ src/lms129/task5.py
@@ -5,8 +5,10 @@ def task5(sentence):
     result = 0
     for i in range(len(sentence)):
         result += 1
+        
     return result
 
 
 if __name__ == "__main__":
     print(task5("I love GBG"))
+    print(task5("i like python"))
