24323f76d03cf1e04e11e53ec7e53ac5ef78e707
sushma Narayana Swamy <susnar0626@edu.lerum.se>
2023-03-14 00:00:24 +0100

    task 9

--- src/lms129/task9.py
+++ src/lms129/task9.py
@@ -2,6 +2,20 @@ from to_do import TODO
 
 
 def task9(temperature):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+    t1 = len(temperature) - 1
+    temp = int(temperature[:t1])
+    unit = (temperature[t1:]).lower()
+    if unit == "f":
+        result = str(int((temp - 32) * 5 / 9)) + "c"
+
+    elif unit == "c":
+        result = str(int((temp * 9 / 5) + 32)) + "F"
+
+    print(result)
+    return result
+
+
+if __name__ == "__main__":
+    task9("-30C")
+    task9("-20C")
+    task9("50f")
